188b3c0064c70eca68a6fc30823b0fef10ec5926
Your Name <you@example.com>
2025-01-22 20:10:44 +0700

    second commit

--- lib/posts.dart
+++ lib/posts.dart
@@ -10,7 +10,6 @@ class PostsPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Posts'),
-        
       ),
       body: FutureBuilder(
         future: httpService.getPosts(),
@@ -22,14 +21,14 @@ class PostsPage extends StatelessWidget {
               itemBuilder: (context, index) {
                 Post post = posts[index];
                 return Padding(
-                  padding: const EdgeInsets.all(8.0), 
+                  padding: const EdgeInsets.all(8.0),
                   child: Container(
                     decoration: BoxDecoration(
                       color: Colors.white,
-                      borderRadius: BorderRadius.circular(12), 
+                      borderRadius: BorderRadius.circular(12),
                       boxShadow: [
                         BoxShadow(
-                          color: Colors.grey.withOpacity(0.5), 
+                          color: Colors.grey.withOpacity(0.5),
                           spreadRadius: 2,
                           blurRadius: 5,
                           offset: Offset(0, 3), // Shadow position
@@ -38,11 +37,11 @@ class PostsPage extends StatelessWidget {
                     ),
                     child: ListTile(
                       leading: CircleAvatar(
-                        backgroundColor: Colors.blueAccent, 
+                        backgroundColor: Colors.blueAccent,
                         child: Text(
-                          '${index + 1}', 
+                          '${index + 1}',
                           style: TextStyle(
-                            color: Colors.white, 
+                            color: Colors.white,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
@@ -52,6 +51,14 @@ class PostsPage extends StatelessWidget {
                         style: TextStyle(fontWeight: FontWeight.bold),
                       ),
                       subtitle: Text("User ID: ${post.userId}"),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => PostDetailPage(post: post),
+                          ),
+                        );
+                      },
                     ),
                   ),
                 );
@@ -68,3 +75,48 @@ class PostsPage extends StatelessWidget {
   }
 }
 
+class PostDetailPage extends StatelessWidget {
+  final Post post;
+
+  PostDetailPage({required this.post});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Post Details"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              post.title,
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "User ID: ${post.userId}",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey[700],
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              post.body,
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.black87,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
